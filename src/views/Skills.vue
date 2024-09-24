0b918f0d61afdf3ba3aaf312aec38c17d641f78a
Luuk0510 <0510luuk@gmail.com>
2024-09-24 15:20:00 +0200

    add: text skills

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -4,42 +4,71 @@
 <template>
     <div class="absolute lg:inset-0 justify-center items-center">
         <section class="flex flex-col justify-center items-center lg:my-20 mb-20">
-            <h1 class="text-4xl font-black mb-5">
-                Mijn vaardigheden
-            </h1>
-            <div class="card bg-neutral text-neutral-content lg:w-3/6">
+            <h1 class="text-4xl font-black mb-5">Mijn vaardigheden</h1>
+
+            <h2 class="text-3xl font-bold mb-4">Frontend Vaardigheden</h2>
+            <div class="card bg-neutral text-neutral-content lg:w-3/6 mb-4">
                 <div class="card-body items-center">
-                    <h1 class="card-title text-3xl font-bold">C#</h1>
+                    <h1 class="card-title text-2xl font-black">HTML</h1>
                     <p>
-                        Ik heb uitgebreide ervaring met C# door het werken aan diverse projecten, waaronder het ontwikkelen van console-applicaties en services. Daarnaast heb ik voornamelijk gewerkt met ASP.NET, waarbij ik webapplicaties heb gebouwd die schaalbaar en toekomstgericht zijn. Hierbij heb ik vaak gewerkt met databases zoals Microsoft SQL, waar ik complexe query's en datamanagement heb toegepast. In mijn projecten heb ik me gericht op het optimaliseren van de backend-logica en het bouwen van RESTful APIs, waardoor ik een solide basis heb in C# en de .NET-omgeving.
+                        HTML is de basis van mijn webontwikkelingservaring. Ik gebruik het om de structuur van webpagina's te definiëren, waarbij ik semantische HTML toepas om de inhoud betekenisvoller te maken. Dit helpt gebruikers de pagina's gemakkelijker te begrijpen en te navigeren.
                     </p>
                 </div>
             </div>
-        </section>
-
-        <section class="flex flex-col justify-center items-center lg:my-20 mb-20">
-            <div class="card bg-neutral text-neutral-content lg:w-3/6">
+            <div class="card bg-neutral text-neutral-content lg:w-3/6 mb-4">
                 <div class="card-body items-center">
-                    <h1 class="card-title text-3xl font-bold">PHP</h1>
+                    <h1 class="card-title text-3xl font-bold">CSS</h1>
                     <p>
-                        Tijdens mijn studie op het MBO heb ik mijn eerste ervaring opgedaan met PHP, waar ik de basisprincipes van webontwikkeling heb geleerd. Bij Mobicoach heb ik verder gewerkt met PHP, waarbij ik geavanceerdere technieken heb toegepast in het onderhoud en de uitbreiding van bestaande applicaties. Op het HBO heb ik mijn kennis verder uitgediept door te werken met het Laravel-framework, waarbij ik MySQL-databases heb geïntegreerd om efficiënte en goed gestructureerde webapplicaties te bouwen.
+                        Ik heb een grondige kennis van CSS en vind het belangrijk dat applicaties er goed uitzien en gebruiksvriendelijk zijn. Ik zorg ervoor dat er altijd een duidelijk kleurcontrast is, zodat slechtzienden de inhoud beter kunnen waarnemen. Ik heb ervaring met frameworks zoals Bootstrap, Tailwind CSS, en gebruik momenteel DaisyUI in dit portfolio. Door deze tools te combineren, kan ik aantrekkelijke en responsieve ontwerpen creëren die de gebruikerservaring verbeteren.
                     </p>
                 </div>
             </div>
-        </section>
-
-        <section class="flex flex-col justify-center items-center lg:my-20 mb-20">
-            <div class="card bg-neutral text-neutral-content lg:w-3/6">
+            <div class="card bg-neutral text-neutral-content lg:w-3/6 mb-4">
+                <div class="card-body items-center">
+                    <h1 class="card-title text-3xl font-bold">JavaScript</h1>
+                    <p>
+                        JavaScript stelt me in staat om interactieve en dynamische webapplicaties te bouwen. Ik heb ervaring met moderne frameworks zoals Vue.js, waarmee ik component-gebaseerde toepassingen kan ontwikkelen.
+                    </p>
+                </div>
+            </div>
+            <div class="card bg-neutral text-neutral-content lg:w-3/6 mb-4">
                 <div class="card-body items-center">
                     <h1 class="card-title text-3xl font-bold">Vue.js</h1>
                     <p>
-                        Met Vue.js heb ik dit portfolio ontwikkeld, waarbij ik de flexibiliteit en kracht van deze frontend framework volledig heb benut. Ik heb Vue.js niet alleen zelfstandig gebruikt, maar ook geïntegreerd met Laravel om dynamische en responsieve webapplicaties te maken. Door gebruik te maken van de Composition API en component-gebaseerde structuur, heb ik Vue.js efficiënt ingezet om intuïtieve en gebruiksvriendelijke interfaces te bouwen.
+                        Met Vue.js heb ik dit portfolio ontwikkeld, waarbij ik de kracht van dit frontend framework volledig heb benut. Ik heb Vue.js niet alleen zelfstandig gebruikt, maar ook geïntegreerd met backend-technologieën voor dynamische en responsieve webapplicaties.
+                    </p>
+                </div>
+            </div>
+
+            <h2 class="text-3xl font-bold mb-4">Backend Vaardigheden</h2>
+            <div class="card bg-neutral text-neutral-content lg:w-3/6 mb-4">
+                <div class="card-body items-center">
+                    <h1 class="card-title text-3xl font-bold">C#</h1>
+                    <p>
+                        Ik heb uitgebreide ervaring met C# door het werken aan diverse projecten, waaronder het ontwikkelen van console-applicaties en services. Met ASP.NET bouw ik schaalbare webapplicaties en RESTful APIs.
+                    </p>
+                </div>
+            </div>
+            <div class="card bg-neutral text-neutral-content lg:w-3/6 mb-4">
+                <div class="card-body items-center">
+                    <h1 class="card-title text-3xl font-bold">PHP</h1>
+                    <p>
+                        Tijdens mijn studie heb ik met PHP gewerkt, waarbij ik de basisprincipes van webontwikkeling heb geleerd. Bij Mobicoach heb ik verder gewerkt met het Laravel-framework, waarmee ik MySQL-databases heb geïntegreerd.
+                    </p>
+                </div>
+            </div>
+            <div class="card bg-neutral text-neutral-content lg:w-3/6 mb-4">
+                <div class="card-body items-center">
+                    <h1 class="card-title text-3xl font-bold">Python</h1>
+                    <p>
+                        Mijn ervaring met Python omvat het bouwen van scripts en applicaties voor diverse doeleinden. Deze taal helpt me om efficiënt en effectief te programmeren, vooral in data-analyse en automatisering.
                     </p>
                 </div>
             </div>
         </section>
     </div>
 </template>
+
 
 <style scoped>
 </style>
