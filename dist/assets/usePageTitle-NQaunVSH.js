15a63a6bc23d675e39e35c7fa2040ed306ac89b8
Luuk0510 <0510luuk@gmail.com>
2024-10-05 00:26:03 +0200

    Deploy to gh-pages

---
+++ dist/assets/usePageTitle-NQaunVSH.js
@@ -0,0 +1,1 @@
+import{I as s,A as u}from"./index-WxniF91Y.js";function f(t="Portfolio"){const i=s();u(()=>{var e;const o=(e=i.meta)==null?void 0:e.title;document.title=o?`${t} - ${o}`:t})}export{f as u};
--- dist/assets/usePageTitle-nU6E8RfR.js
+++
@@ -1,1 +0,0 @@
-import{B as s,A as u}from"./index-DoStB4vt.js";function f(t="Portfolio"){const i=s();u(()=>{var e;const o=(e=i.meta)==null?void 0:e.title;document.title=o?`${t} - ${o}`:t})}export{f as u};
