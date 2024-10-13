4f370e7fb9e3e6f37be3e478ddc44cf1a186ebef
Luuk0510 <0510luuk@gmail.com>
2024-10-13 16:32:53 +0200

    Deploy to gh-pages

---
+++ dist/assets/Mobicoach-CjbCwUez.js
@@ -0,0 +1,1 @@
+import{_ as o,o as c,c as t,a}from"./index-sGA4x8rW.js";const s={};function n(r,e){return c(),t("div",null,e[0]||(e[0]=[a("h1",{class:"text-4xl font-black"},"Mobicoach",-1)]))}const f=o(s,[["render",n]]);export{f as default};
