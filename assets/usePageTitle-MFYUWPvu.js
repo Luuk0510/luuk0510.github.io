3b301c08e260a09e8b1d9b317b4127f97d055d27
Luuk0510 <0510luuk@gmail.com>
2025-02-20 22:40:57 +0100

    Deploy to gh-pages

--- assets/usePageTitle-MFYUWPvu.js
+++
@@ -1,1 +0,0 @@
-import{I as s,z as u}from"./index-BJrjmWE-.js";function f(t="Portfolio"){const i=s();u(()=>{var e;const o=(e=i.meta)==null?void 0:e.title;document.title=o?`${t} - ${o}`:t})}export{f as u};
