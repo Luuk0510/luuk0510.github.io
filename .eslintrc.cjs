b26d628514a00a407b3860664ad417f6ca05fa01
Luuk <0510luuk@gmail.com>
2025-08-21 23:16:57 +0200

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,21 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser', // <— laat TypeScript in .vue werken
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    extraFileExtensions: ['.vue'],
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:@typescript-eslint/recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
   rules: {
     'vue/multi-word-component-names': 0,
+    'no-undef': 'off', // TS regelt dit al in .vue/.ts
   },
 }
