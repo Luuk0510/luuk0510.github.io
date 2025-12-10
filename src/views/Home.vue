7fe006a2197fe9671ad73d7b10e1d378e04cd13d
Luuk <0510luuk@gmail.com>
2025-12-11 00:31:13 +0100

    Move hero logos into shared data module

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,39 +8,11 @@ import IconDownArrow from '@/components/icons/IconDownArrow.vue';
 import IconGitHub from '@/components/icons/IconGitHub.vue';
 import IconLinkedIn from '@/components/icons/IconLinkedIn.vue';
 import { useHero } from '@/composables/useHero';
-
-import HTML5Logo from '@/assets/images/logo/html5_logo.png';
-import CSS3Logo from '@/assets/images/logo/css3_logo.png';
-import JavaScriptLogo from '@/assets/images/logo/javascript_logo.png';
-import TailwindLogo from '@/assets/images/logo/tailwind_logo.png';
-import VueLogo from '@/assets/images/logo/vue_logo.png';
-import SQLLogo from '@/assets/images/logo/sql_logo.png';
-import CSharpLogo from '@/assets/images/logo/csharp_logo.png';
-import PythonLogo from '@/assets/images/logo/python_logo.png';
-import PHPLogo from '@/assets/images/logo/php_logo.png';
-import LaravelLogo from '@/assets/images/logo/laravel_logo.png';
-import FlutterLogo from '@/assets/images/logo/flutter_logo.png';
+import { heroLogos } from '@/data/logos';
 
 const { displayedText, isTyping, showBlinkingCursor, showButton, linksAnimated, scrollToItem } = useHero();
 
-interface Logo {
-    src: string;
-    alt: string;
-}
-
-const logos = ref<Logo[]>([
-    { src: HTML5Logo, alt: "HTML logo" },
-    { src: CSS3Logo, alt: "CSS logo" },
-    { src: JavaScriptLogo, alt: "JavaScript logo" },
-    { src: TailwindLogo, alt: "Tailwind logo" },
-    { src: VueLogo, alt: "Vue logo" },
-    { src: SQLLogo, alt: "SQL logo" },
-    { src: CSharpLogo, alt: "C Sharp logo" },
-    { src: PythonLogo, alt: "Python logo"},
-    { src: PHPLogo, alt: "PHP logo" },
-    { src: LaravelLogo, alt: "Laravel logo" },
-    { src: FlutterLogo, alt: "Flutter logo" },
-]);
+const logos = ref(heroLogos);
 </script>
 
 
