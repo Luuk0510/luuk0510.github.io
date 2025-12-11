c59912ea5604a29c65c9957ed30784a986bfa752
Luuk <0510luuk@gmail.com>
2025-12-11 10:23:20 +0100

    add title to experience

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,12 +1,15 @@
-<script setup>
+<script setup lang="ts">
+import { usePageTitle } from '@/composables/usePageTitle'
+
+usePageTitle();
 </script>
 
 <template>
-    <section class="h-screen">
-        <h1 class="text-center text-4xl font-bold">
-            Werkervaring
-        </h1>
-    </section>
+    <div class="lg:inset-0 justify-center items-center">
+        <div class="flex flex-col justify-center items-center mt-24 mb-20">
+            <h1 class="text-4xl font-black mb-5">Werkervaring</h1>
+        </div>
+    </div> 
 </template>
 
 <style scoped></style>
