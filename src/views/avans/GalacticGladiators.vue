c2cbb824c66b5243d9f065e35b52a5ac824c6c18
Luuk <0510luuk@gmail.com>
2025-12-11 19:16:09 +0100

    Use BaseCard on Avans pages and centralize project images

--- src/views/avans/GalacticGladiators.vue
+++ src/views/avans/GalacticGladiators.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
 import { usePageTitle } from '@/composables/usePageTitle'
+import BaseCard from '@/components/BaseCard.vue'
 
 usePageTitle();
 </script>
 
 <template>
-    <section class="flex flex-col justify-center items-center mt-24 mb-20">
+    <section class="flex flex-col justify-center items-center mt-24 mb-20 px-4">
         <h1 class="text-4xl font-black mb-5">
             Galactic Gladiators
         </h1>
-        <div class="card bg-neutral text-neutral-content lg:w-3/6">
-            <div class="card-body items-center">
-                <p>
-                </p>
-            </div>
-        </div>
+        <BaseCard
+            :motion="false"
+            cardClass="bg-neutral text-neutral-content lg:w-3/6"
+            bodyClass="items-center"
+        >
+            <p>
+                Hier komt meer informatie over Galactic Gladiators.
+            </p>
+        </BaseCard>
     </section>        
 </template>
 
