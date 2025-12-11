b8b4149d90aaed2448bf7c0486f6d66f9f9c21f3
Luuk <0510luuk@gmail.com>
2025-12-11 11:06:07 +0100

    add animations

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -7,27 +7,34 @@ usePageTitle();
 
 <template>
     <div class="lg:inset-0 justify-center items-center">
-        <div class="flex flex-col justify-center items-center mt-24 mb-20">
+        <div class="flex flex-col justify-center items-center mt-24 mb-20 gap-8">
             <h1 class="text-4xl font-black mb-5">Werkervaring</h1>
-            sep 2024 - jan 2025
-            <BaseCard cardClass="bg-neutral text-neutral-content lg:w-3/6 shadow-xl" bodyClass="items-center">
-                <p class="text-4xl font-black mb-5 text-center">
-                    Marvelous
-                </p>
-                <p>
-                    Placeholder
-                </p>
-            </BaseCard>
-            <br>
-            feb 2021 - aug 2024
-            <BaseCard cardClass="bg-neutral text-neutral-content lg:w-3/6 shadow-xl" bodyClass="items-center">
-                <p class="text-4xl font-black mb-5 text-center">
-                    Mobicoach
-                </p>
-                <p>
-                    Placeholder
-                </p>
-            </BaseCard>
+
+            <div class="flex flex-col items-center gap-2">
+                <p class="font-mono italic">sep 2024 - jan 2025</p>
+                <BaseCard :motion="true" :delay="800" :duration="800"
+                    cardClass="bg-neutral text-neutral-content lg:w-3/6 shadow-xl" bodyClass="items-center">
+                    <p class="text-4xl font-black mb-5 text-center">
+                        Marvelous
+                    </p>
+                    <p>
+                        Hier komt een tekst over de stage bij Marvelous en wat ik heb gedaan.
+                    </p>
+                </BaseCard>
+            </div>
+
+            <div class="flex flex-col items-center gap-2">
+                <p class="font-mono italic">feb 2021 - aug 2024</p>
+                <BaseCard :motion="true" :delay="900" :duration="800"
+                    cardClass="bg-neutral text-neutral-content lg:w-3/6 shadow-xl" bodyClass="items-center">
+                    <p class="text-4xl font-black mb-5 text-center">
+                        Mobicoach
+                    </p>
+                    <p>
+                        Hier komt tekst over de stage en part time werk bij Mobicaoch en wat ik heb gedaan.
+                    </p>
+                </BaseCard>
+            </div>
         </div>
     </div>
 </template>
