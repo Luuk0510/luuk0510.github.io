18269d3cb357db32fb76512ae2f04c855aae10b0
Luuk <0510luuk@gmail.com>
2025-12-11 18:51:19 +0100

    update text

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,7 +9,8 @@ export const experienceItems: ExperienceItem[] = [
     title: 'Marvelous',
     dates: 'sep 2024 - jan 2025',
     description: [
-      'Tijdens mijn HBO-stage bij Marvelous heb ik gewerkt met C# en hun eigen Backlot-framework. Ik ontwikkelde functionaliteit binnen hun interne webapplicatie en leerde werken binnen een grotere codebasis. Ik heb veel geleerd over samenwerken en communiceren binnen een professioneel ontwikkelteam. Daarnaast heb ik ervaring opgedaan met plannen, het bijhouden van taken en het werken met sprints.',
+      'Tijdens mijn HBO-stage bij Marvelous heb ik gewerkt met C# en hun eigen Backlot-framework. Marvelous is een Nederlands digitaal bureau dat webapplicaties en platforms ontwikkelt en optimaliseert met .NET-technologie voor organisaties die hun digitale dienstverlening willen verbeteren.',
+      'Ik ontwikkelde functionaliteit binnen hun interne webapplicatie en leerde werken binnen een grotere codebasis. Ik heb veel geleerd over samenwerken en communiceren binnen een professioneel ontwikkelteam. Daarnaast heb ik ervaring opgedaan met plannen, het bijhouden van taken en het werken met sprints.',
       'Deze stage heeft mijn technische kennis verdiept en mijn professionele houding en werkstructuur versterkt.',
     ],
   },
@@ -17,7 +18,8 @@ export const experienceItems: ExperienceItem[] = [
     title: 'Mobicoach',
     dates: 'feb 2021 - aug 2024',
     description: [
-      'Tijdens mijn MBO-opleiding heb ik twee keer stage gelopen bij Mobicoach. Na deze stages heb ik daar ook parttime gewerkt. Ik werkte aan interne websystemen die dagelijks door het team werden gebruikt.',
+      'Tijdens mijn MBO-opleiding heb ik twee keer stage gelopen bij Mobicoach. Mobicoach is een Nederlands softwarebedrijf dat oplossingen ontwikkelt voor fleetmanagement, waarmee transportbedrijven hun voertuigen en logistieke processen efficiënt kunnen beheren.',
+      'Na deze stages heb ik daar ook parttime gewerkt. Ik werkte aan interne websystemen die dagelijks door het team werden gebruikt.',
       'In deze periode heb ik veel geleerd over samenwerken en duidelijk communiceren binnen een klein ontwikkelteam. Bij Mobicoach heb ik mijn basis in webontwikkeling verder versterkt en geleerd om gestructureerd en zelfstandig te werken.',
       'Deze ervaring heeft een belangrijke rol gespeeld in mijn ontwikkeling en vormt een goede basis voor mijn huidige opleiding.',
     ],
